6d61aa05a559eb96d24d2d1ae1ef008909297815
onikarubi <printuserselect@gmial.com>
2020-12-03 09:24:08 +0900

    algorithm js

---
+++ Documents/programming/algorithm/js/al.js
@@ -0,0 +1,30 @@
+class Algorithm {
+  constructor() {
+    this.n = 0
+  }
+
+  FizzBuzz(n) {
+    if(n % 3 == 0 && n % 5 == 0) {
+      return 'fizzbuzz'
+  
+    } else if(n % 3 == 0){
+      return 'fizz'
+  
+    } else if(n % 5 == 0) {
+      return 'buzz'
+  
+    } else {
+      return n
+    }
+  }
+  
+  DoFB() {
+    for(let i = 1; i <= 10; i++) {
+      console.log(this.FizzBuzz(i))
+    }
+  }
+}
+const fizzbuzz = new Algorithm()
+fizzbuzz.DoFB()
+
+
